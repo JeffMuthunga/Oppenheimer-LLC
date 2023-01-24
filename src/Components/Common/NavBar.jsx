4171bb3a035baf8dde9784465703022502344ebd
Jeff Muthunga <jeff.wainaina@student.moringaschool.com>
2023-01-24 18:07:59 +0300

    Completed Background CSS

---
+++ src/Components/Common/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {NavLink} from "react-router-dom"
+
+function NavBar (){
+
+    return (
+        <header>
+        <nav className="nav-container">
+        <div className="navbar">
+            <NavLink className="nav-branding" href="#">
+                <img src="./Images/axis.png" alt="Oppenheimer-logo"/>
+                Oppenheimer LLC</NavLink>
+            <ul className="nav-menu">
+                <li className="nav-link"><NavLink  to="/"> Home</NavLink ></li>
+                <li className="nav-link"><NavLink  to="/">Display Properties</NavLink ></li>
+                <li className="nav-link"><NavLink  to="/">Favorites</NavLink ></li>
+            </ul>
+        </div>
+        </nav>
+        </header>
+    )
+}
+
+export default NavBar
---
+++ src/Components/Pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Home () {
+
+    return (
+        <div>
+        <div className="container">
+            <h1>Oppenheimer LLC</h1> 
+            
+        </div>
+        <div>
+            <h4>wow haha</h4>
+        </div>
+        </div>
+    )
+}
+
+export default Home
